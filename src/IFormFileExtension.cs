58c102eb45cf2825480e3a5a5d2a33e6edee863e
Jake Soenneker <jake@soenneker.com>
2026-01-08 13:58:23 -0600

    MemoryStreamUtil

--- src/IFormFileExtension.cs
+++ src/IFormFileExtension.cs
@@ -2,6 +2,9 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Soenneker.Extensions.Stream;
+using Soenneker.Extensions.Task;
+using Soenneker.Extensions.ValueTask;
+using Soenneker.Utils.MemoryStream.Abstract;
 
 namespace Soenneker.Extensions.IFormFile;
 
@@ -25,8 +28,33 @@ public static class IFormFileExtension
     public static async ValueTask<MemoryStream> ToMemoryStream(this Microsoft.AspNetCore.Http.IFormFile formFile, CancellationToken cancellationToken = default)
     {
         var memoryStream = new MemoryStream();
-        await formFile.CopyToAsync(memoryStream, cancellationToken).ConfigureAwait(false);
+        await formFile.CopyToAsync(memoryStream, cancellationToken)
+                      .NoSync();
         memoryStream.ToStart();
         return memoryStream;
+    }
+
+    /// <summary>
+    /// Asynchronously copies the contents of the specified form file to a new memory stream using the provided memory
+    /// stream utility.
+    /// </summary>
+    /// <remarks>The returned memory stream's position is set to 0. The caller is responsible for disposing
+    /// the returned stream when it is no longer needed.</remarks>
+    /// <param name="formFile">The form file whose contents will be copied to the memory stream. Cannot be null.</param>
+    /// <param name="memoryStreamUtil">An implementation of a memory stream utility used to obtain the target memory stream. Cannot be null.</param>
+    /// <param name="cancellationToken">A cancellation token that can be used to cancel the asynchronous operation.</param>
+    /// <returns>A task that represents the asynchronous operation. The result contains a memory stream with the form file's
+    /// contents, positioned at the beginning.</returns>
+    public static async ValueTask<MemoryStream> ToMemoryStream(this Microsoft.AspNetCore.Http.IFormFile formFile, IMemoryStreamUtil memoryStreamUtil,
+        CancellationToken cancellationToken = default)
+    {
+        MemoryStream stream = await memoryStreamUtil.Get(cancellationToken)
+                                                    .NoSync();
+
+        await formFile.CopyToAsync(stream, cancellationToken)
+                      .NoSync();
+
+        stream.Position = 0;
+        return stream;
     }
 }
