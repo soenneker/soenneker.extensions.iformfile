3080679afeae4ed40c6ae8b86affc2d853555620
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:58 -0500

    TUnit conversion

--- test/Soenneker.Extensions.IFormFile.Tests/IFormFileExtensionTests.cs
+++ test/Soenneker.Extensions.IFormFile.Tests/IFormFileExtensionTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Extensions.IFormFile.Tests;
 
 public class IFormFileExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
